73468084b089a7b6a2651508936dca1e6c332933
Bart van Sevenbergen <bartvansevenbergen@hotmail.com>
2021-11-15 18:12:20 +0100

    small changes

--- rpi/raspotify-event.sh
+++ rpi/raspotify-event.sh
@@ -1,7 +1,8 @@
-if [ "$PLAYER_EVENT" = "start" ]; then
+if [ "$PLAYER_EVENT" = "stop" ]
+then
+  sudo python3 python/startstop/stop.py
+elif [ "$PLAYER_EVENT" = "start" ]
+then
   sudo python3 python/startstop/start.py
 fi
 
-if [ "$PLAYER_EVENT" = "stop" ]; then
-  sudo python3 python/startstop/stop.py
-fi
